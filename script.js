f1fe0ce83715bb61dda383ff742577af60118901
BChaturved <98033194+BChaturved@users.noreply.github.com>
2022-05-17 20:59:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var names=["john","yakov","chatur","sammu","krish"];
+ for (var i =0; i<names.length;i++)
+ 	{
+           if(names[i].charAt(0)=='j'|| names[i].charAt(0)=='J')
+           {
+                		console.log("Goodbye"+ names[i]);
+           }
+           else{
+               console.log("hello"+ names[i]);
+           }
+ 	}
